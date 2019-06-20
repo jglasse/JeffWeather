53678ba970b26e2c4612b9760cc298fbc005eba6
jglasse <jeff@appetizeapp.com>
2019-06-20 11:19:58 -0700

    Initial commit. Just getting things set up

--- JeffWeather/AppDelegate.swift
+++ JeffWeather/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreLocation
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- JeffWeather/JeffViewController.swift
+++ JeffWeather/JeffViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class JeffWeatherViewController: UIViewController {
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
